d1194f8296a13b5a8f895ecb5e3fa501fa5e33ec
Abhi-chintu <abhishekabhi4842@gmail.com>
2025-08-27 19:33:24 +0530

    Bastion host code added

---
+++ eks-with-terraform/bastion.tf
@@ -0,0 +1,61 @@
+resource "aws_instance" "bastion" {
+  ami           = var.bastion_ami
+  instance_type = var.bastion_instance_type
+  subnet_id     = aws_subnet.public-subnet[0].id
+  vpc_security_group_ids = [aws_security_group.bastion-sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.project}-bastion-host"
+  }
+  
+}
+
+resource "aws_security_group" "bastion-sg" {
+  name        = "${var.project}-bastion-sg"
+  description = "Security group for bastion host"
+  vpc_id      = aws_vpc.vpc.id
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = tolist([var.vpc_cidr])
+  }
+
+  tags = {
+    Name = "${var.project}-bastion-sg"
+  }
+}
+
+resource "aws_iam_role" "ssm-role" {
+  name = "${var.project}-bastion-ssm-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ssm-managed-policy" {
+  role       = aws_iam_role.ssm-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+resource "aws_iam_role_policy_attachment" "ssm-profile" {
+  role       = aws_iam_role.ssm-role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+}
+
+resource "aws_iam_instance_profile" "bastion-ssm-profile" {
+  name = "${var.project}-bastion-ssm-profile"
+  role = aws_iam_role.ssm-role.name
+}
